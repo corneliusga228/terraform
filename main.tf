75ee0fbc59ff8e89b9fac04fcba3f7e1869dda5f
corneliusga228 <75066163+corneliusga228@users.noreply.github.com>
2023-02-28 12:39:33 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "CostarCapstone"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_dynamodb_table" "listings"{
+  name = "Listings"
+  hash_key = "ListingID"
+  
+  attribute {
+    name = "ListingId"
+    type = "N"
+  }
+}
+resource "aws_dynamodb_table" "extracted-keys"{
+  name = "extracted-keys"
+  hash_key = "ListingID"
+  
+  attribute {
+    name = "ListingId"
+    type = "N"
+  }
+}
+resource "aws_dynamodb_table" "openai-prompt-result"{
+  name = "Listings"
+  hash_key = "ListingID"
+  
+  attribute {
+    name = "ListingId"
+    type = "N"
+  }
+}
