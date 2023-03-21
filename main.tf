55f03d4ca6cd93824bf2323999b7791d2853e52d
Gabrielle <corneliusga@mymail.vcu.edu>
2023-03-21 13:54:49 -0400

    Testing terraform from console

--- main.tf
+++ main.tf
@@ -21,11 +21,11 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1
 }
 
 resource "aws_dynamodb_table" "listings-terraform"{
-  name = "Listings"
+  name = "Listings-test"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
@@ -34,104 +34,5 @@ resource "aws_dynamodb_table" "listings-terraform"{
   attribute {
     name = "ListingId"
     type = "N"
-  }
-}
-resource "aws_dynamodb_table" "extracted-keys-terraform"{
-  name = "extracted-keys"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "ListingId"
-  
-  attribute {
-    name = "ListingId"
-    type = "N"
-  }
-}
-resource "aws_dynamodb_table" "openai-prompt-result-terraform"{
-  name = "Listings"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "ListingId"
-  
-  attribute {
-    name = "ListingId"
-    type = "N"
-  }
-}
-provider "archive" {}
-data "archive_file" "zip" {
-  type        = "zip"
-  source_file = "welcome.py"
-  output_path = "welcome.zip"
-}
-data "aws_iam_policy_document" "policy" {
-  statement {
-    sid    = ""
-    effect = "Allow"
-    principals {
-      identifiers = ["lambda.amazonaws.com"]
-      type        = "Service"
-    }
-    actions = ["sts:AssumeRole"]
-  }
-}
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.policy.json
-}
-resource "aws_lambda_function" "proxy-lambda" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
-  filename      = "lambda_function_payload.zip"
-  function_name = "proxy-lambda"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.test"
-
-  source_code_hash = data.archive_file.lambda.output_base64sha256
-
-  runtime = "nodejs16.x"
-
-  environment {
-    variables = {
-      foo = "bar"
-    }
-  }
-}
-resource "aws_lambda_function" "openai-prompt" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
-  filename      = "lambda_function_payload.zip"
-  function_name = "openai-prompt"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.test"
-
-  source_code_hash = data.archive_file.lambda.output_base64sha256
-
-  runtime = "nodejs16.x"
-
-  environment {
-    variables = {
-      foo = "bar"
-    }
-  }
-}
-resource "aws_lambda_function" "openai_gpt" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
-  filename      = "lambda_function_payload.zip"
-  function_name = "openai_gpt"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.test"
-
-  source_code_hash = data.archive_file.lambda.output_base64sha256
-
-  runtime = "nodejs16.x"
-
-  environment {
-    variables = {
-      foo = "bar"
-    }
   }
 }
