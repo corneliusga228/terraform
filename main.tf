12cb5f7d230c86103d4e64eec3871bf4ce4a5d97
corneliusga228 <75066163+corneliusga228@users.noreply.github.com>
2023-03-28 13:36:36 -0400

    Update main.tf
    Added backend configuration

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "remote" {
+    organization = "CostarCapstone"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
